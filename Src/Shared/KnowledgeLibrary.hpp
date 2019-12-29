a702d4817667eefd02152d9207f98d7c628de864
Zheng Yingwei <dtcxzyw@qq.com>
2019-12-29 13:52:56 +0800

    12.29

--- Src/Shared/KnowledgeLibrary.hpp
+++ Src/Shared/KnowledgeLibrary.hpp
@@ -1,16 +1,15 @@
 #pragma once
 #include "Config.hpp"
 
-class KnowledgePoint :public Bus::ModuleFunctionBase {
+class KnowledgeLibrary :public Bus::ModuleFunctionBase {
 protected:
-    explicit KnowledgePoint(Bus::ModuleInstance &instance)
+    explicit KnowledgeLibrary(Bus::ModuleInstance &instance)
         : ModuleFunctionBase(instance) {}
 
 public:
     static Name getInterface() {
-        return "NCEEHelper.Knowledge";
+        return "NCEEHelper.KnowledgeLibrary";
     }
 
     virtual GUID load(std::shared_ptr<Config> cfg) = 0;
-    virtual bool test() = 0;
 };
