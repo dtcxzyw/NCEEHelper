39d4c5eceeb101874261ca75005b40fdaf0c56b9
Zheng Yingwei <dtcxzyw@qq.com>
2020-03-19 13:23:59 +0800

    3.19

--- Script/Spider/People.py
+++ Script/Spider/People.py
@@ -26,7 +26,7 @@ def getHtmlImpl(address):
     while True:
         time.sleep(0.05)
         header = {
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3833.98 Safari/537.36"}
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"}
         return requests.get(address, headers=header, allow_redirects=True).content.decode("gbk")
 
 
